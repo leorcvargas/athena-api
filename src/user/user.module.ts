631206e0b45d2bdfa282d8c412ea2f22ff655f04
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2022-01-27 22:08:53 -0300

    Remove unnecessary user controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
-import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 import { CreateUserService } from './create-user.service';
 import { FindUserService } from './find-user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UserController],
   providers: [FindUserService, UserResolver, CreateUserService],
   exports: [FindUserService, CreateUserService],
 })
