c92ae57dd77c0069dd968a60200ccf0183b99164
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2022-02-02 15:19:49 -0300

    Add: Logout mutation

--- src/auth/login.resolver.ts
+++ src/auth/login.resolver.ts
@@ -1,10 +1,16 @@
-import { ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Resolver, Mutation, Args, Context } from '@nestjs/graphql';
 import { CookieOptions, Request, Response } from 'express';
+import { ResponsePayload } from 'src/lib/gql/response.payload';
 
 import { LoginInput } from './dto/login.input';
 import { LoginPayload } from './dto/login.payload';
+import { GqlAuthGuard } from './guards/graphql-auth.guard';
 import { LoginService } from './login.service';
 
 @Resolver()
@@ -33,12 +39,32 @@ export class LoginResolver {
     return { user, accessToken };
   }
 
+  @Mutation((_returns) => ResponsePayload)
+  @UseGuards(GqlAuthGuard)
+  async logout(@Context() context: { req: Request; res: Response }) {
+    const cookie = this.buildLogoutAuthCookie();
+    context.res.cookie(cookie.name, cookie.value, cookie.options);
+
+    const response = new ResponsePayload();
+    response.success = true;
+
+    return response;
+  }
+
+  private buildLogoutAuthCookie() {
+    const name = this.configService.get<string>('app.authCookie');
+    const value = '';
+    const options: CookieOptions = {};
+
+    return { name, value, options };
+  }
+
   private buildAuthCookie(accessToken: string) {
     const name = this.configService.get<string>('app.authCookie');
-    const value = `Bearer ${accessToken}`;
+    const value = accessToken;
     const options: CookieOptions = {
       domain: this.configService.get<string>('domain'),
-      maxAge: Date.now() + 7 * 24 * 3600000, // 7 days,
+      maxAge: 7 * 24 * 3600000, // 7 days,
     };
 
     return { name, value, options };
